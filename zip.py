af9634c851f1ea9610bac4f513272eca185be14e
KennethCarlos <157500602+KennethCarlos@users.noreply.github.com>
2024-05-30 23:45:53 +0200

    zip.py

--- zip.py
+++ zip.py
@@ -1,5 +1,6 @@
+#we use zip to unite two arrays
 cores=["amarelo", "verde", "azul",  "vermelho"]
 valor= [10, 20, 30, 40]
  
 listas=zip(cores, valor)
-print(list(listas))
+print(list(listas))#to print a zip we have to transforme it in a list
