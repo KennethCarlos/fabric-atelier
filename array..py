3d7cdb5656204de9df2af24d63b5e55f65546bc1
KennethCarlos <157500602+KennethCarlos@users.noreply.github.com>
2024-05-31 22:47:06 +0200

    array..py

--- array..py
+++ array..py
@@ -1,10 +1,11 @@
+
 from array import array
 
 letras=["a", "b", "c", "d"]
 ints=[10, 20, 30, 40]
 floats=[1.2, 2.2, 3.2]
 
-letras=array("u", letras)
+letras=array("u", letras) # adds leter in the end of thr arrai
 ints=array("i", ints)
 floats=array("f", floats)
 
