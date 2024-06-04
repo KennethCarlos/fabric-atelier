a0dc84fd6f8fb4f08ba2fea70c2f22a7671b8e9e
KennethCarlos <157500602+KennethCarlos@users.noreply.github.com>
2024-06-04 16:11:37 +0200

    Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -12,3 +12,13 @@ def mulp(x):
     return f(x)*4
 
 print(mulp(2))
+
+####################
+numeros=[1, 2, 3, 4, 5, 6]
+quadrados=lambda num: num**2
+resultados=[]
+
+for i in numeros:
+    resultados.append(quadrados(i))
+
+print(resultados)
